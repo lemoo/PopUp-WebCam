a3ae9660897d6ec0bfd95debc8855698a8519640
lemoo <lemueldecastro@gmail.com>
2021-10-19 10:45:26 -0300

    Create PopWebCam.js

---
+++ PopWebCam.js
@@ -0,0 +1,92 @@
+function doc_keyUp(e) {
+
+    // this would test for whichever key is 40 (down arrow) and the ctrl key at the same time
+    if (e.ctrlKey && e.key === 'ArrowUp') {
+        // call your function to do the thing
+	var video = document.querySelector('#cam');	
+       	video.requestPictureInPicture();
+    }
+
+	if (e.ctrlKey && e.key === 'ArrowDown') {
+        // call your function to do the thing
+	var video = document.querySelector('#cam');	
+       	document.exitPictureInPicture();
+    }
+}
+
+let add_element = () => {
+    const template = document.createElement('div');
+    template.innerHTML = `
+	<style>@keyframes spin { 100% {transform: rotateZ(360deg);}} </style>
+	<div id="mybox"
+	style="position:fixed; z-index:99999; bottom:5%; right:5%; 
+	//box-shadow: 0 0 20px rgba(0,0,0,0.5); 
+	border-radius:50%; 
+	width:266px; height:266px;" >
+	<div style="z-index:-1; border-radius:50%; position:absolute; height:266px; width:266px; 
+	//background-image: linear-gradient(rgba(88, 43, 217, 0.75),rgba(88, 43, 217, 1));
+	background-image: linear-gradient(rgba(255,255,255,0.25),rgba(255,255,255,0),rgba(255,255,255,0.25),rgba(88, 43, 217, 0.9));
+	animation: spin 5s linear infinite;"></div>
+	<video id="cam"  style="border-radius:50%; object-fit:cover; height:256px; width:256px; margin-top:5px; margin-left:5px;transform: rotateY(180deg);"></video>
+	</div>
+    `;
+     document.body.appendChild(template);	// adding element to the body.
+}
+
+navigator.mediaDevices.getUserMedia({
+  video: true
+})
+
+.then(function (stream) {
+  var video = document.querySelector('#cam');
+  video.srcObject = stream;
+  video.play();
+})
+
+
+function dragElement(elmnt) {
+//let dragElement = (elmnt) => {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+  
+  
+    function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+  
+}
+add_element();
+dragElement(document.getElementById("mybox"));
+document.addEventListener('keyup', doc_keyUp, false);
